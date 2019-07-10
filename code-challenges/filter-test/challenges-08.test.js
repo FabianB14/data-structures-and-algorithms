96f917cd63d0285a1d53227090dfcdd8337288dc
FabianB14 <32110808+FabianB14@users.noreply.github.com>
2019-07-10 16:03:26 -0700

    fixed weather

--- code-challenges/filter-test/challenges-08.test.js
+++ code-challenges/filter-test/challenges-08.test.js
@@ -27,6 +27,8 @@ For example, filterStringsWithVowels('gregor','hound','xyz') returns ['gregor', 
 
 const filterStringsWithVowels = (arr) => {
   // Solution code here...
+  const results = arr.filter(val => val.match(/[aeiou]/g))
+  return results;
 };
 
 
@@ -173,7 +175,7 @@ Run your tests from the console: jest challenges-08.test.js
 
 ------------------------------------------------------------------------------------------------ */
 
-describe('Testing challenge 1', () => {
+xdescribe('Testing challenge 1', () => {
   test('It should return an array containing only odd integers', () => {
     expect(oddValues([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).toStrictEqual([1, 3, 5, 7, 9]);
     expect(oddValues([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]).length).toStrictEqual(5);
@@ -195,7 +197,7 @@ xdescribe('Testing challenge 2', () => {
   });
 });
 
-xdescribe('Testing challenge 3', () => {
+describe('Testing challenge 3', () => {
   const firstNums = [1, 2, 3];
   const secondNums = [1, 2, 3, 4];
 

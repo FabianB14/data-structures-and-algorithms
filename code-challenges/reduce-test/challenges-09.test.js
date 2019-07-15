5a4daebd825e6df6eb183c67c6b44263234748d8
FabianB14 <32110808+FabianB14@users.noreply.github.com>
2019-07-15 08:50:18 -0700

    for checkout

--- code-challenges/reduce-test/challenges-09.test.js
+++ code-challenges/reduce-test/challenges-09.test.js
@@ -10,6 +10,7 @@ Note: You may not use the array's built-in length property.
 
 const countNumberOfElements = (arr) => {
   // Solution code here...
+  
 };
 
 /* ------------------------------------------------------------------------------------------------

b4d66918c65c3a39f7aa8c1292d8d0737a33ebf1
Brooks <fabbrook@amazon.com>
2019-08-07 13:58:11 -0700

    few more changes

---
+++ code401challenges/src/test/java/code401challenges/ArrayShiftTest.java
@@ -0,0 +1,20 @@
+package code401challenges;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ArrayShiftTest {
+
+    @Test
+    public void arrayShift() {
+        int[] arr2 = new int[]{1,2,3,5,6};
+        int[] arr = new int[]{1,2,4,5};
+        int[] expectedArr = new int[]{1,2,3,4,5};
+        int num2 = 4;
+        int num = 3;
+        ArrayShift testArrayShift = new ArrayShift();
+        Assert.assertEquals(testArrayShift.arrayShift(arr,3), expectedArr);
+    }
+}

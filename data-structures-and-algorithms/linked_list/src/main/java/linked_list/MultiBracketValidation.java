97032b45f181965cec8c8fd7cf4d57f1b0e39bcb
FabianB14 <32110808+FabianB14@users.noreply.github.com>
2019-08-22 16:13:18 -0700

    this is a update

--- data-structures-and-algorithms/linked_list/src/main/java/linked_list/MultiBracketValidation.java
+++ data-structures-and-algorithms/linked_list/src/main/java/linked_list/MultiBracketValidation.java
@@ -1,4 +1,39 @@
 package linked_list;
 
-public class MulitBracketValidation {
+public class MultiBracketValidation{
+    public static boolean multiBracketValidation(String input){
+        Stack<Character> stack = new Stack<>();
+        Character checker;
+        for(int i = 0; i < input.length(); i++){
+            if (input.charAt(i) == '(' || input.charAt(i) == '{' || input.charAt(i) == '[') {
+               stack.push(input.charAt(i));
+            }
+            switch (input.charAt(i)) {
+                case')':
+                     checker = stack.pop();
+                    if( checker != '('){
+                        return false;
+                    }
+                    break;
+                case ']':
+                     checker = stack.pop();
+                    if( checker != '['){
+                        return false;
+                    }
+                    break;
+                case '}':
+                    checker = stack.pop();
+                    if( checker != '{'){
+                        return false;
+                    }
+                    break;
+
+            }
+        }
+        if(stack.top != null){
+            return false;
+        }
+        System.out.println(stack.peek());
+        return true;
+    }
 }

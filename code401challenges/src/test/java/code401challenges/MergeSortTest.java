a66fe3490f69af9ac3062274a4bd68242ea4b9fe
FabianB14 <32110808+FabianB14@users.noreply.github.com>
2019-09-11 12:28:36 -0700

    this is for inserstion sort

--- code401challenges/src/test/java/code401challenges/MergeSortTest.java
+++ code401challenges/src/test/java/code401challenges/MergeSortTest.java
@@ -1,5 +1,21 @@
+package code401challenges;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class MergeSortTest {
 
+    @Test
+    public void mergeSorter() {
+        int[] actual = { 3, 1, 7, 2, 5, 4, 6 };
+        int[] expected = { 1, 2, 3, 4, 5, 6, 7 };
+        MergeSort.mergeSorter(actual, actual.length);
+        assertArrayEquals(expected, actual);
+
+        int[] actual2 = { 4, 1, 3, 2};
+        int[] expected2 = { 1, 2, 3, 4 };
+        MergeSort.mergeSorter(actual, actual.length);
+        assertArrayEquals(expected, actual);
+    }
 }
